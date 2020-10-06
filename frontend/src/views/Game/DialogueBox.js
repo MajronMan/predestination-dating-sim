4cdd6bc812c1cd9cbd05f718bf4cb0dcc93bd883
Miron Markowski <majron15@gmail.com>
2020-10-06 10:00:53 +0200

    Enable navigation through dialogue

--- frontend/src/views/Game/DialogueBox.js
+++ frontend/src/views/Game/DialogueBox.js
@@ -13,19 +13,44 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class DialogueBox extends React.Component {
+  state = { currentStatement: null };
   componentDidMount() {
     this.props.getDialogue();
-    
   }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.dialogue && this.props.dialogue) {
+      this.setState({ currentStatement: this.props.dialogue._statements[0] });
+    }
+  }
+
+  progressDialogue = (answer) => () => {
+    const newStatement = this.props.dialogue._statements.find(
+      (x) => x._id === answer._next
+    );
+    this.setState({ currentStatement: newStatement });
+  };
+
   render() {
-    const {dialogue} = this.props
-    const st = (dialogue && dialogue._statements[0]) || {_text: "", _answers: []}
+    if (!this.props.dialogue || !this.state.currentStatement) {
+      return null;
+    }
+
+    const { currentStatement } = this.state;
+
     return (
       <div className="container">
-        <p>{st._text}</p>
+        <p>{currentStatement._text}</p>
         <div className="answers">
-          {st._answers.map((a, i) => (
-            <p key={`dialogue_option_${i}`}>{"> " + a._text}</p>
+          {currentStatement._answers.map((a, i) => (
+            <p key={`dialogue_option_${i}`}>
+              <a
+                style={{ cursor: "pointer" }}
+                onClick={this.progressDialogue(a)}
+              >
+                {"> " + a._text}
+              </a>
+            </p>
           ))}
         </div>
       </div>
